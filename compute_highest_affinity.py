deb5da2ee9e26fa1963f78947eaf4114e3152eba
utssd <shida.shen@utexas.edu>
2017-12-02 15:54:22 -0600

    add py file

---
+++ compute_highest_affinity.py
@@ -0,0 +1,27 @@
+def highest_affinity(site_list, user_list, time_list):
+
+    n = len(site_list)
+    if n == 0:
+        print("empty")
+        return ()
+    if len(user_list) != n or len(time_list) != n:
+        print("not same length")
+        return ()
+    record = dict()
+    for i in range(n):
+        if site_list[i] in record:
+            if user_list[i] not in record[site_list[i]]:
+                record[site_list[i]].append(user_list[i])
+        else:
+            record[site_list[i]] = [user_list[i]]
+    max_aff = -1
+    for key in record:
+        for another_key in record:
+            if key != another_key:
+                common = [f for f in record[key] if f in record[another_key]]
+                if len(common) > max_aff:
+                    max_aff = len(common)
+                    max_pair = (
+                        another_key, key) if another_key < key else (
+                        key, another_key)
+    return max_pair
